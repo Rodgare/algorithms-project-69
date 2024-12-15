08180fa2f624ce005d63b8c5ef1f541fca7703a5
Axel <axel19911@gmail.com>
2024-12-15 17:15:37 +0000

    lint

--- src/index.php
+++ src/index.php
@@ -4,43 +4,45 @@ namespace App;
 
 function tokenize(string $text): array
 {
-  return preg_split('/\W+/', strtolower($text), -1, PREG_SPLIT_NO_EMPTY);
+    return preg_split('/\W+/', strtolower($text), -1, PREG_SPLIT_NO_EMPTY);
 }
 
 function buildIndex($docs, $q)
 {
-  $index = [];
-  $tokQ = tokenize($q);
-  foreach ($docs as $doc) {
-    $count = array_count_values(tokenize($doc['text']));
-    foreach ($tokQ as $token) {
-
-      if (isset($count[$token]) && isset($index[$doc['id']])) {
-        $index[$doc['id']] += $count[$token];
-      } else if (isset($count[$token])) {
-        $index[$doc['id']] = $count[$token];
-      }
-      
+    $index = [];
+    $tokQ = tokenize($q);
+    foreach ($docs as $doc) {
+        $count = array_count_values(tokenize($doc['text']));
+        foreach ($tokQ as $token) {
+            if (isset($count[$token]) && isset($index[$doc['id']])) {
+                $index[$doc['id']] += $count[$token];
+            } elseif (isset($count[$token])) {
+                $index[$doc['id']] = $count[$token];
+            }
+        }
     }
-  }
 
-  return $index;
+    return $index;
 }
 
 function calcIDF($docs, $q)
 {
-  $index = buildIndex($docs, $q);
-  if ($index === []) return [];
-  $idf = log10(count($docs) / count($index));
-  foreach ($index as $docId => $tf) {
-    $index[$docId] = $tf * $idf;
-  }
-  arsort($index);
-  return array_keys($index);
+    $index = buildIndex($docs, $q);
+    if ($index === []) {
+        return [];
+    }
+    $idf = log10(count($docs) / count($index));
+    foreach ($index as $docId => $tf) {
+        $index[$docId] = $tf * $idf;
+    }
+    arsort($index);
+    return array_keys($index);
 }
 
 function search($docs, $q)
 {
-  if ($q === '') return [];
-  return calcIDF($docs, $q);
+    if ($q === '') {
+        return [];
+    }
+    return calcIDF($docs, $q);
 }
